c3d99c93dade79b0a9a263307e8b564bc74f278c
Bhaskar <reddybhaskar437@gmail.com>
2021-03-02 22:18:21 -0600

    added controllers

---
+++ controllers/locationController.js
@@ -0,0 +1,17 @@
+/**
+ * Location controller
+ *  Handles requests related to location (see routes)
+ *
+ * @author Bhaskar reddy Minupuri <S538310@nwmissouri.edu>
+ */
+
+// import dependencies
+const express = require('express');
+
+// import local code files
+// const model = require('../models/location.js');
+
+// create a router
+const router = express.Router();
+
+module.exports = router;
