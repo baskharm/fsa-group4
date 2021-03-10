9327a0da8042e41fc463f90148cd98422e851ca7
rajeshwari rudravaram <rajeshwarihyd.02@gmail.com>
2021-03-09 20:35:15 -0600

    add Procfile and update location

---
+++ init-locations.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        Name: 'Village O Apartments',
+        type: 'circle',
+        coordinates:
+            [
+                {
+                    latitude: 33.4271871,
+                    longitude: -111.8848516
+                },
+                {
+                    latitude: 33.4271852,
+                    longitude: -111.8848570
+                },
+                {
+                    latitude: 33.4271822,
+                    longitude: -111.8848589
+                }
+            ]
+    },
+    {
+        Name: 'Horizons West Apartments',
+        type: 'circle',
+        coordinates:
+            [
+                {
+                    latitude: 41.31447748801441,
+                    longitude: -95.05660917970772
+                },
+                {
+                    latitude: 41.31447748801235,
+                    longitude: -95.05660917970880
+                },
+                {
+                    latitude: 41.31447748801211,
+                    longitude: -95.05660917970970
+                }
+            ]
+    }
+
+];
