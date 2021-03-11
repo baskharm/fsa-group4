f04cf2de53d838862605920e15c1692d75d6f26b
rajeshwari rudravaram <rajeshwarihyd.02@gmail.com>
2021-03-10 21:02:25 -0600

    added index-setup to models

---
+++ models/index-setup.js
@@ -0,0 +1,26 @@
+/**
+ * Sequelize database configurer.
+ *
+ * Returns a default function that applies higher-level
+ * database configuration aspects, such as associations between
+ * models.
+ *
+ * @link See: https://sequelize.org/master/manual/assocs.html
+ *
+ * @param {*} sequelize
+ */
+
+module.exports = async (sequelize) => {
+    const {
+        Location,
+        Clue,
+
+    } = await sequelize.models;
+
+
+    // each location can have one or more clues
+    Location.hasMany(Clue);
+    Clue.belongsTo(Location);
+
+
+};
