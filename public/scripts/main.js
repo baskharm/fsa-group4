cd5867a01fbd9e8d6d62021dd6268a67335f6810
Bhaskar <reddybhaskar437@gmail.com>
2021-03-25 15:30:32 -0500

    updated main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,11 @@
 import { isInsideCircle } from "./location-circle.js";
-import locationsArray from "./init-locations.js"
+import locationsArray from "./locations.js";
 
-let device, location;
+
+// let device, location;
 let colorElement1 = document.getElementById("status");
 let colorElement2 = document.getElementById("status1");
 
-function main() {
-    console.log('Page is fully loaded');
-}
 
 window.addEventListener('load', main);
 colorElement1.addEventListener('click', onClickSquareBox1);
@@ -15,43 +13,133 @@ colorElement1.addEventListener('touch', onClickSquareBox1);
 colorElement2.addEventListener('click', onClickSquareBox2);
 colorElement2.addEventListener('touch', onClickSquareBox2);
 
+function main() {
+    console.log('Page is fully loaded');
+}
 
 async function onClickSquareBox1() {
-    location = locationsArray[0];
-    let confirmation = "Your target location is " + location.name;
-    document.getElementById("status1").innerHTML = confirmation;
-    let utterance = new SpeechSynthesisUtterance(confirmation);
+    document.getElementById("targetloc").innerHTML = "The Treasure is in the location ";
+    document.getElementById("lbl").innerHTML = targetLoc.Name;
+    document.getElementById("device-lat1").innerHTML = targetLoc.coordinates[0].latitude;
+    document.getElementById("device-long1").innerHTML = targetLoc.coordinates[0].longitude;
+    let utterance = new SpeechSynthesisUtterance(`The location where the treasure is ${targetLoc.Name}`);
     speechSynthesis.speak(utterance);
+
+
+}
+//     location = locationsArray[0];
+//     let confirmation = "Your target location is " + location.name;
+//     document.getElementById("status1").innerHTML = confirmation;
+//     let utterance = new SpeechSynthesisUtterance(confirmation);
+//     speechSynthesis.speak(utterance);
+// }
+
+// async function onClickSquareBox2() {
+//     device = await getLocation();
+
+//     let isInside = isInsideCircle(device, location);
+//     let status;
+//     let speak;
+//     status = "Device Coordinates: " + "<br>";
+//     status += "Latitude: " + device.coords.latitude + "<br>";
+//     status += "Longitude: " + device.coords.longitude + "<br>";
+//     if (isInside) {
+//         status += "Congratulations!! You have reached the destination: " + location.name;
+//         speak = "Congratulations!! You have reached the destination: " + location.name;
+//     } else {
+//         status += "You haven't reached the destination";
+//         speak = "You haven't reached the destination";
+//     }
+//     document.getElementById("status2").innerHTML = status;
+//     let utterance = new SpeechSynthesisUtterance(speak);
+//     speechSynthesis.speak(utterance);
+// }
+
+// // collects current location
+// async function getLocation() {
+//     return new Promise((resolve, reject) => {
+//         navigator.geolocation.getCurrentPosition(resolve, reject);
+//     }).then((position) => {
+//         return position;
+//     });
+// }
+function onClickSquareBox2() {
+    document.getElementById("targetloc").innerHTML = "The Treasure is in the location ";
+    document.getElementById("lbl").innerHTML = targetLoc.Name;
+    document.getElementById("device-lat1").innerHTML = targetLoc.coordinates[0].latitude;
+    document.getElementById("device-long1").innerHTML = targetLoc.coordinates[0].longitude;
+    let utterance = new SpeechSynthesisUtterance(`The location where the treasure is ${targetLoc.Name}`);
+    speechSynthesis.speak(utterance);
+
+
 }
 
-async function onClickSquareBox2() {
-    device = await getLocation();
 
-    let isInside = isInsideCircle(device, location);
-    let status;
-    let speak;
-    status = "Device Coordinates: " + "<br>";
-    status += "Latitude: " + device.coords.latitude + "<br>";
-    status += "Longitude: " + device.coords.longitude + "<br>";
-    if (isInside) {
-        status += "Congratulations!! You have reached the destination: " + location.name;
-        speak = "Congratulations!! You have reached the destination: " + location.name;
-    } else {
-        status += "You haven't reached the destination";
-        speak = "You haven't reached the destination";
-    }
-    document.getElementById("status2").innerHTML = status;
-    let utterance = new SpeechSynthesisUtterance(speak);
-    speechSynthesis.speak(utterance);
-}
-
-// collects current location
 async function getLocation() {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject);
-    }).then((position) => {
+    }).then(position => {
         return position;
     });
+}
+
+let currentlat, currentlon, loc, error = true;
+let targetLoc = locationsArray[Math.floor(Math.random()*locationsArray.length)];
+
+async function onClickSquareBox1() {
+    const locText = await getLocation();
+    // loc = 'Your current Location';
+    // document.getElementById("location").innerHTML = loc;
+    currentlat = locText.coords.latitude;
+    console.log(currentlat)
+    document.getElementById("device-lat").innerHTML = currentlat.toFixed(6);
+    currentlon = locText.coords.longitude;
+    console.log(currentlon)
+    document.getElementById("device-long").innerHTML = currentlon.toFixed(6);
+    
+    locationsArray.forEach(function (value) {
+        if (isInside(value.Latitude, value.Longitude)) {
+            document.getElementById("location").innerHTML = value.Name;
+            let utterance = new SpeechSynthesisUtterance("Congratulations!, You found location ${value.Name}");
+            speechSynthesis.speak(utterance);
+            error = false;
+        }
+    });
+
+    if(error) {
+
+        console.log("error is here")
+        document.getElementById("error-message").innerHTML = "Sorry,You're not near to the treasure";
+         let utterance = new SpeechSynthesisUtterance("Sorry,You're not near to the treasure");
+         speechSynthesis.speak(utterance);
+     } else {
+         document.getElementById("error-message").innerHTML = "";
+     }
+    
+    }
+    function isInside(questLat, questLon) {
+        let distance = distanceBetweenLocations(questLat, questLon);
+        console.log("distance: " + distance);
+        if (distance < 30) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+function distanceBetweenLocations(questLat, questLon) {
+    const R = 6371e3;
+    const φ1 = currentlat * Math.PI / 180;
+    const φ2 = questLat * Math.PI / 180;
+    const Δφ = (questLat - currentlat) * Math.PI / 180;
+    const Δλ = (questLon - currentlon) * Math.PI / 180;
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const d = R * c;
+    return d; 
 }
 
 
