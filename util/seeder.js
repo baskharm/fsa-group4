b24fbe045aa99847827b3dffbc867522484101a7
rajeshwari rudravaram <rajeshwarihyd.02@gmail.com>
2021-04-03 18:50:36 -0500

    update locations in seeder

--- util/seeder.js
+++ util/seeder.js
@@ -1,46 +1,58 @@
-const LOG = require('./logger');
+/**
+ * Seed the database with sample data.
+ *
+ * During development, drop & recreate the database on startup.
+ *
+ * Only as we move into production (and the app is stable) will we
+ * begin to store real data.
+ *
+ * *
+ */
+
+const LOG = require("./logger");
 
 module.exports = async (db) => {
-  LOG.info('Starting seeder.......................');
+  LOG.info("Starting seeder.......................");
+
   try {
     const syncResult = await db.sync({ force: true });
     LOG.info(`Recreated all tables: ${syncResult}`);
   } catch (err) {
     LOG.error(`ERROR: on sync process - ${err.message}`);
   }
-
-
   try {
     await db.models.Location.bulkCreate(
       [
-        // list of locations
-        { locationId: 1, 
-          locationName: 'Colden Pond',
-          locationLatitude: 40.35112,
-          locationLongitude: -94.88220
+        // list of locations........
+        {
+          locationId: 1,
+          locationName: 'Bearcat football stadium',
+          locationLatitude: 40.3515,
+          locationLongitude: -94.8826
         },
-        { locationId: 2,
-          locationName: 'Horizons West Apartments',
-          locationLatitude:41.31447 ,
-          locationLongitude: -95.05660
-         },
-        { locationId: 3, 
-          locationName: 'B.D Owens Library' ,
-          locationLatitude: 40.35380,
-          locationLongitude: -94.88598
-        }
+        {
+          locationId: 2,
+          locationName: "Colden Pond",
+          locationLatitude: 40.3511,
+          locationLongitude: -94.882,
+        },
+        {
+          locationId: 3,
+          locationName: "Bearcat Soccer field",
+          locationLatitude: 40.3513,
+          locationLongitude: -94.8828,
+        },
 
+      ],
+      { validate: true } // add options object to call new model validators
+    );
+    const num = await db.models.Location.count();
+    LOG.info(`Seeded ${num} locations.`);
+  } catch (err) {
+    LOG.error(`ERROR: Location seeding - ${err.message}`);
+  }
 
-    ],
-    { validate: true } // add options object to call new model validators
-  );
-  const num = await db.models.Location.count();
-  LOG.info(`Seeded ${num} locations.`);
-} catch (err) {
-  LOG.error(`ERROR: Location seeding - ${err.message}`);
-}
-
-LOG.info('Done with seeder................');
+  LOG.info("Done with seeder................");
 
   return db;
 };
