1e2bfa44daa5830d7f68204e3f0acb55b3806d63
rajeshwari rudravaram <rajeshwarihyd.02@gmail.com>
2021-04-05 12:25:33 -0500

    add manifest.json and servier-worker.js files

---
+++ public/scripts/load-service-worker.js
@@ -0,0 +1,13 @@
+/** 
+*
+* @author Dr.Case
+* @modified by Rajeshwari
+*
+*/
+    // If service workers are supported by user browser
+    // AFter all content has loaded, register the service worker
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+          navigator.serviceWorker.register('./scripts/service-worker.js')
+        })
+      }
