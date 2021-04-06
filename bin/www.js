1407acf643ca9bf36da68a9f2b66156ce9908e00
Bhaskar <reddybhaskar437@gmail.com>
2021-04-06 12:03:44 -0500

    Adding postgresql code.

--- bin/www.js
+++ bin/www.js
@@ -97,10 +97,27 @@ const onListening = async () => {
     await require('../util/seeder')(db);
   } catch (err) {
     LOG.error(`ERROR with database:${err.message}`);
+    updatePostgreSequences(db);
   }
   const addr = server.address();
   const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
   LOG.info(`Listening on ${bind}`);
+};
+
+/* Workaround for increment indexes of postgre not updating after seeding by manually setting them */
+const updatePostgreSequences = async (db) => {
+  if (process.env.NODE_ENV !== "production") return;
+  const extistingLocations = await db.models.location.count();
+  const extistingCoordinates = await db.models.coordinate.count();
+
+  await db.queryInterface.sequelize.query(
+    `ALTER SEQUENCE "locations_id_seq" RESTART WITH ${extistingLocations + 1}`
+  );
+  // await db.queryInterface.sequelize.query(
+  //   `ALTER SEQUENCE "coordinates_id_seq" RESTART WITH ${
+  //     extistingCoordinates + 1
+  //   }`
+  // );
 };
 
 /**
