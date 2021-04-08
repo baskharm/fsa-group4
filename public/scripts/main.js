1649f8e92ca001d6991025bcd1fea0a59d49867d
Bhaskar <reddybhaskar437@gmail.com>
2021-04-08 17:00:35 -0500

    added some methods to main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -19,3 +19,25 @@ colorElement.addEventListener("click", onClickSquareBox2);
 colorElement.addEventListener("touch", onClickSquareBox2);
 colorElement1.addEventListener("click", onClickSquareBox1);
 colorElement1.addEventListener("touch", onClickSquareBox1);
+
+async function onClickSquareBox1() {
+    $.ajax({
+      type: "GET",
+      url: "/location/findRandom",
+      success: function (coord) {
+        targetLoc = coord;
+        document.getElementById("targetloc").innerHTML =
+          "The treasure location is ready..! ";
+        document.getElementById("lbl").innerHTML = " Start playing the game.";
+        let utterance = new SpeechSynthesisUtterance(
+          `The treasure location is ready start playing the game.`
+        );
+        speechSynthesis.speak(utterance);
+        document.getElementById("device-lat").innerHTML =
+          targetLoc.locationLatitude;
+        document.getElementById("device-long").innerHTML =
+          targetLoc.locationLongitude;
+      },
+    });
+  }
+  
