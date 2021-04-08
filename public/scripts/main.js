2cb0d2933d21400d104ddb791c109154aa507e4d
Bhaskar <reddybhaskar437@gmail.com>
2021-04-08 17:01:28 -0500

    updated the code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -40,4 +40,29 @@ async function onClickSquareBox1() {
       },
     });
   }
+  async function getLocation() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    }).then((position) => {
+      return position;
+    });
+  }
   
+  async function onClickSquareBox2() {
+    if (!targetLoc) return;
+    const locText = await getLocation();
+  
+    [currentlat, currentlon] = [
+      locText.coords.latitude,
+      locText.coords.longitude,
+    ];
+  
+    // Uncomment to test
+    //   [currentlat, currentlon] = [40.3515, -94.8826];
+  
+    document.getElementById("error-message").innerHTML = "";
+    document.getElementById("location").innerHTML = "";
+    document.getElementById("device-lat").innerHTML = `Your location: `;
+    document.getElementById("device-long").innerHTML = `(${currentlat.toFixed(
+      5
+    )}, ${currentlon.toFixed(5)})`;  
